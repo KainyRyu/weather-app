03eb4e685271af8a9f148c0f40a68913fe4d8602
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-05 14:41:42 +0000

    Displaying the information

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const api = {
+  key: '9b683aa46e3e257910433cfadb9afd58',
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+  const [searchLocation, setSearchLocation] = useState('')
+// 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="search-box">
+          <input 
+            className="search-bar" 
+            type="text"
+            placeholder="Search the location..."
+            value={searchLocation}
+            onChange={(e) => setSearchLocation(e.target.value)}
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">London, GB</div>
+          {/* <div className="location-date">{dateBuilder(new Date)}</div> */}
+        </div>
+        <div className="weather-box">
+          <div className="temp">10°C</div>
+          <div className="weather">Sunny</div>
+        </div>
+      </div>
     </div>
   );
 }
